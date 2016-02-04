7e8e77b984dd2417ad5a1cef6b3fbbf5cb6e7242
Rob Worsnop <rworsnop@gmail.com>
2016-02-03 19:19:54 -0500

    Used InstanceRunner

--- src/main/java/io/vertx/example/Server.java
+++ src/main/java/io/vertx/example/Server.java
@@ -1,15 +1,21 @@
 package io.vertx.example;
 
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.file.FileSystem;
+import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.shareddata.SharedData;
+import io.vertxbeans.InstanceRunner;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Created by Rob Worsnop on 9/5/15.
@@ -32,7 +38,11 @@ public class Server {
     private Handler<HttpServerRequest> someHandler;
 
     @PostConstruct
-    public void start(){
-        vertx.createHttpServer().requestHandler(someHandler).listen(8080);
-    }
+    public void start() throws InterruptedException, ExecutionException, TimeoutException {
+        // Create two instances
+        InstanceRunner.executeBlocking(2,
+                (Handler<AsyncResult<HttpServer>> handler) ->
+                    vertx.createHttpServer().requestHandler(someHandler).listen(8080, handler),
+                    1, TimeUnit.MINUTES);
+     }
 }
