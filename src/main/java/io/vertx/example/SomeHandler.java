d9b7409f6f6d5487bd7b43cb20575c2f5ce80219
Rob Worsnop <rworsnop@gmail.com>
2016-08-04 10:07:40 -0400

    Added docker stuff

--- src/main/java/io/vertx/example/SomeHandler.java
+++ src/main/java/io/vertx/example/SomeHandler.java
@@ -2,6 +2,7 @@ package io.vertx.example;
 
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,8 +10,12 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SomeHandler implements Handler<HttpServerRequest>{
+
+    @Value("${display.message:Hello World 123!}")
+    private String message;
+
     @Override
     public void handle(HttpServerRequest request) {
-        request.response().end("Hello World!");
+        request.response().end(message);
     }
 }
